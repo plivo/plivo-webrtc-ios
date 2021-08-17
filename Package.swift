d9b852227738ec9b88939861b2fd83cc1d44de93
Krishna Kumar <krishna.kumar@plivo.com>
2021-08-17 20:52:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,31 +6,18 @@ import PackageDescription
 let package = Package(
     name: "PlivoVoiceWebRTC",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PlivoVoiceWebRTC",
-            targets: ["PlivoVoiceWebRTC"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "WebRTC",
+            targets: ["WebRTC"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PlivoVoiceWebRTC",
-            dependencies: []),
         .binaryTarget(
-                    name: "WebRTC",
-                    url: "https://s3.us-west-1.amazonaws.com/helpers.plivo.com/ios/WebRTC.xcframework.zip",
-                    checksum: "88f2a6f111e4fa8f8d7e671407c33d1e15621c2a86a9c33ab43d40d4497eab96"
-        ),
-        .testTarget(
-            name: "PlivoVoiceWebRTCTests",
-            dependencies: ["PlivoVoiceWebRTC"]),
+            name: "WebRTC",
+            url: "https://s3.us-west-1.amazonaws.com/helpers.plivo.com/ios/WebRTC.xcframework.zip",
+            checksum: "88f2a6f111e4fa8f8d7e671407c33d1e15621c2a86a9c33ab43d40d4497eab96"
+        )
     ]
 )
