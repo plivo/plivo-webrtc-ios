507a3da4c1208b52ba8d66d5527c5679e25b08a7
Krishna Kumar <krishna.kumar@plivo.com>
2021-08-17 19:50:39 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
             name: "PlivoVoiceWebRTC",
             dependencies: []),
         .binaryTarget(
-                    name: "WebRTC.xcframework",
+                    name: "WebRTC",
                     url: "https://s3.us-west-1.amazonaws.com/helpers.plivo.com/ios/WebRTC.xcframework.zip",
                     checksum: "88f2a6f111e4fa8f8d7e671407c33d1e15621c2a86a9c33ab43d40d4497eab96"
         ),
