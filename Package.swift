80bf61a4b4f77fba876876353ab97c202c8ced47
Krishna Kumar <krishna.kumar@plivo.com>
2021-08-19 13:49:55 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "WebRTC",
             url: "https://s3.us-west-1.amazonaws.com/helpers.plivo.com/ios/WebRTC.xcframework.zip",
-            checksum: "88f2a6f111e4fa8f8d7e671407c33d1e15621c2a86a9c33ab43d40d4497eab96"
+            checksum: "716b80d7ab67dfc619a426c07daee44ce69e485ef0164d6eb3c5d8efdee245c6"
         )
     ]
 )
